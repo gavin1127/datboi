6ac332ee15c69d93aaecf47044f68570026542e8
Rickie Cashwell <rjc6056@gmail.com>
2017-03-01 17:23:14 -0500

    dat boi again

---
+++ src/Java1.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Gavin on 3/1/17.
+ */
+import java.util.*;
+public class Java1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+
+        String input = scan.nextLine();
+
+        System.out.println();
+
+        if(input.equals("Rickie")){
+            System.out.println("Ye boi Rickie");
+        }
+		
+
+
+        }
+
+    }
+
+
